573f3af7ea77e0327fd73401ed33f73225508e64
yusuf mohamed <yusuf.mohamed.work@gmail.com>
2026-03-16 21:38:21 +0300

    added the user struct

---
+++ services/user/internal/application/core/domain/user.go
@@ -0,0 +1,14 @@
+package domain
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        uint      `json:"id"`  // this is local id
+	Sub       string    `json:"sub"` // this is id from providers(in case user changes email, the sub will be always be the same)
+	Provider  string    `json:"provider"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
