63cbd97b6f3b5dec93490d2e1f665a9fb3b88a08
yusuf mohamed <yusuf.mohamed.work@gmail.com>
2026-03-21 14:33:40 +0300

    updated the chat table

--- services/chat/internal/adapters/secondary/postgresql/chat.go
+++ services/chat/internal/adapters/secondary/postgresql/chat.go
@@ -1,16 +1,22 @@
 package postgresql
 
 import (
+	"github.com/Yusufdot101/ribble/services/chat/internal/application/core/domain"
 	"gorm.io/gorm"
 )
 
-type User struct {
-	gorm.Model
-	ChatID uint
-	UserID uint
-}
-
 type Chat struct {
 	gorm.Model
-	Users []User `gorm:"constraint:OnDelete:CASCADE;"`
+	Participants []ChatParticipant `gorm:"constraint:OnDelete:CASCADE;"`
+	Messages     []Message         `gorm:"constraint:OnDelete:CASCADE;"`
+}
+
+func (a *Adapter) InsertChat(chat *domain.Chat) error {
+	chatModel := &Chat{}
+
+	res := a.db.Save(chatModel)
+	if res.Error == nil {
+		chat.ID = chatModel.ID
+	}
+	return res.Error
 }
---
+++ services/chat/internal/adapters/secondary/postgresql/chat_integration_test.go
@@ -0,0 +1,11 @@
+package postgresql
+
+import "github.com/Yusufdot101/ribble/services/chat/internal/application/core/domain"
+
+func (rts *RepositoryTestSuite) TestInsertChat() {
+	adapter, err := NewAdapter(rts.dataSourceURL)
+	rts.Require().Nil(err)
+	chat := domain.NewChat()
+	err = adapter.InsertChat(chat)
+	rts.Nil(err)
+}
