7da600a25dbf18d828960208cca28f7a9be88b25
yusuf mohamed <yusuf.mohamed.work@gmail.com>
2026-04-11 17:19:36 +0300

    added skeleton loading for users

---
+++ frontend/components/UserCardSkeleton.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useState } from "react";
+
+interface Props {
+    index: number;
+}
+
+const nameSizes = [30, 40, 50, 60, 75];
+const emailSizes = [10, 20, 30, 50, 60, 75];
+
+const UserCardSkeleton = ({ index }: Props) => {
+    const [nameSize] = useState(
+        () => nameSizes[Math.floor(Math.random() * nameSizes.length)],
+    );
+    const [emailSize] = useState(
+        () => emailSizes[Math.floor(Math.random() * emailSizes.length)],
+    );
+    return (
+        <div
+            className={`${index === 0 ? "" : "border-t-[1px]"} border-foreground p-[4px] pointer-events-none duration-300 flex flex-col gap-y-[4px] h-[64px] duration-300`}
+        >
+            <div
+                style={{ width: `${nameSize}%` }}
+                className="h-full bg-foreground/75"
+            ></div>
+            <div
+                style={{ width: `${emailSize}%` }}
+                className="h-full bg-foreground/75"
+            ></div>
+        </div>
+    );
+};
+
+export default UserCardSkeleton;
