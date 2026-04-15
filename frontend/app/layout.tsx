30991aa31f685b466d5a2d9c078a7022fedaf6a4
yusuf mohamed <yusuf.mohamed.work@gmail.com>
2026-04-15 06:01:59 +0300

    removed footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
             >
                 <Header />
                 <div className="flex-1">{children}</div>
-                <Footer />
+                {/* <Footer /> */}
             </body>
         </html>
     );
