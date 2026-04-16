81ffdecb252b7e3a819420958da3dd25fa07ee41
yusuf mohamed <yusuf.mohamed.work@gmail.com>
2026-04-16 18:24:33 +0300

    fixed merge conflict

--- services/chat/internal/ports/chat_service.go
+++ services/chat/internal/ports/chat_service.go
@@ -4,6 +4,10 @@ import "github.com/Yusufdot101/ripple/services/chat/internal/application/core/do
 
 type ChatService interface {
 	NewChatWithParticipants(userIDs []uint) (*domain.Chat, error)
+<<<<<<< Updated upstream
 	GetChatParticipants(chatID uint) ([]*domain.ChatParticipant, error)
+=======
+	GetChatByUserIDs(userIDs []uint) (*domain.Chat, error)
+>>>>>>> Stashed changes
 	NewMessage(userID, chatID uint, content string) error
 }
