23e96101667e117eb13fd104d520a2a738448785
Yusuf Mohamed <yusuf.mohamed.work@gmail.com>
2026-05-06 20:36:44 +0300

    updated the messages handler to use DTO

--- frontend/utils/permissions.ts
+++ frontend/utils/permissions.ts
@@ -4,18 +4,24 @@ export type PermissionType = {
     id: number;
     name: string;
 };
+
+type GetUserPermissionsResponse = {
+    permissions: PermissionType[];
+};
 export const getUserPermissions = async (
     chatID: number,
-): Promise<PermissionType[] | undefined> => {
+): Promise<GetUserPermissionsResponse> => {
     try {
         // /chats/:chatId/permissions
         const res = await api(
             `${BASE_CHAT_SERVICE_API_URL}/chats/${chatID}/permissions`,
         );
-        if (!res) return;
-        const data = await res.json();
-        return data.permissions;
+        if (!res) return { permissions: [] };
+        const body = await res.json();
+        const data = body.data;
+        return data;
     } catch (error) {
         console.error(error);
+        return { permissions: [] };
     }
 };
--- services/chat/internal/adapters/primary/api/permission_handlers.go
+++ services/chat/internal/adapters/primary/api/permission_handlers.go
@@ -5,16 +5,22 @@ import (
 	"strconv"
 
 	"github.com/Yusufdot101/ripple/services/chat/internal/adapters/primary/api/context"
+	"github.com/Yusufdot101/ripple/services/chat/internal/adapters/primary/api/response"
+	"github.com/Yusufdot101/ripple/services/chat/internal/application/core/domain"
 	"github.com/gin-gonic/gin"
 )
+
+type Permissions struct {
+	Permissions []*domain.Permission `json:"permissions"`
+}
 
 func (h *handler) getUserPermissions(c *gin.Context) {
 	currentUserID := context.UserIDFromContext(c)
 
 	chatID, err := strconv.ParseUint(c.Param("chatId"), 10, strconv.IntSize)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "invalid chat id",
+		c.JSON(http.StatusBadRequest, response.Response[any]{
+			Error: "invalid chat id",
 		})
 		return
 	}
@@ -22,13 +28,15 @@ func (h *handler) getUserPermissions(c *gin.Context) {
 
 	permissions, err := h.csvc.GetUserPermissions(chatIDUint, currentUserID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "error getting permissions",
+		c.JSON(http.StatusInternalServerError, response.Response[any]{
+			Error: "error getting permissions",
 		})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"permissions": permissions,
+	c.JSON(http.StatusOK, response.Response[Permissions]{
+		Data: Permissions{
+			Permissions: permissions,
+		},
 	})
 }
